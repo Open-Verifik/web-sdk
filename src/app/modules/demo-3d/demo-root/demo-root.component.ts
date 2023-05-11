f22e03073b8b91479a009d9a0f3c6f0c1642aacb
daniel <dgallardoc@outlook.com>
2023-05-11 13:13:20 -0500

    ageestimate

--- src/app/modules/demo-3d/demo-root/demo-root.component.ts
+++ src/app/modules/demo-3d/demo-root/demo-root.component.ts
@@ -77,6 +77,7 @@ export class DemoRootComponent implements OnInit {
     faceScan: any;
     idScan: any;
     currentImg:any;
+    ageEstimate: any;
 
     constructor(
         private _formBuilder: FormBuilder,
