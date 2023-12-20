6cf1a6dbe48afe59b8f32201318d305d94f1c47c
Miguel Trevino <miguel@verifik.co>
2023-12-20 10:15:20 -0600

    compile access

--- src/app/modules/auth/sign-up/sign-up.routes.ts
+++ src/app/modules/auth/sign-up/sign-up.routes.ts
@@ -1,9 +1,9 @@
-import { Routes } from '@angular/router';
-import { AuthSignUpComponent } from 'app/modules/auth/sign-up/sign-up.component';
+import { Routes } from "@angular/router";
+import { AuthSignUpComponent } from "app/modules/auth/sign-up/sign-up.component";
 
 export default [
-    {
-        path     : '',
-        component: AuthSignUpComponent,
-    },
+	{
+		path: ":id",
+		component: AuthSignUpComponent,
+	},
 ] as Routes;
