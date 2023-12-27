0650ff30c8336bd42ecb8f4b320944099c4afb42
migueltrevinom <miguel@chefsurf.io>
2023-12-27 15:07:36 -0500

    compile changes for sign in

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
 	baseUrl: "/",
-	apiUrl: "https://verifik.app",
+	apiUrl: "/",
 	appUrl: "https://verifik.app",
 	kycUrl: "https://sandbox-kyc.verifik.co",
 	verifikProject: "6266193db77ccc8111730c90",
