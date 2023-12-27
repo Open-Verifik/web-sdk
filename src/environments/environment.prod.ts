5d2e3eef9434a126869c075ea220e36da2896188
migueltrevinom <miguel@chefsurf.io>
2023-12-27 15:14:22 -0500

    dist changes for the env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 	baseUrl: "/",
-	apiUrl: "/",
+	apiUrl: "",
 	appUrl: "https://app.verifik.co",
 	kycUrl: "https://kyc.verifik.co",
 	verifikProject: "6332941ccde4f719d9c00f9e",
