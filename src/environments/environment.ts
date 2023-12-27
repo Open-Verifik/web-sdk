9af13e4bb57beff463fd35e4679f449a426bde36
migueltrevinom <miguel@chefsurf.io>
2023-12-27 15:11:32 -0500

    compile env for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
 	baseUrl: "/",
-	apiUrl: "/",
+	apiUrl: "https://verifik.app",
 	appUrl: "https://verifik.app",
 	kycUrl: "https://sandbox-kyc.verifik.co",
 	verifikProject: "6266193db77ccc8111730c90",
