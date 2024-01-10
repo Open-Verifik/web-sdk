cd0f1cde1859a1da278bb9d070359d7625cf71a0
Miguel Trevino <miguel@verifik.co>
2024-01-10 13:40:16 -0500

    compile redirection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,6 @@ export const environment = {
 	appUrl: "https://app.verifik.co",
 	kycUrl: "https://kyc.verifik.co",
 	verifikProject: "6332941ccde4f719d9c00f9e",
+	sandboxUrl: "https://sandbox.verifik.co",
+	sandboxProject: "6266193db77ccc8111730c90",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,4 +5,6 @@ export const environment = {
 	appUrl: "https://verifik.app",
 	kycUrl: "https://sandbox-kyc.verifik.co",
 	verifikProject: "6266193db77ccc8111730c90",
+	sandboxUrl: "https://sandbox.verifik.co",
+	sandboxProject: "6266193db77ccc8111730c90",
 };
