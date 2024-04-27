4f03f32cbf36d207ab00e6e456d3552abe83df3a
Miguel Treviño <miguel@verifik.co>
2024-04-27 16:16:14 -0500

    change url for apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
 	baseUrl: "/",
-	apiUrl: "http://localhost:3001",
+	apiUrl: "https://verifik.app",
 	appUrl: "https://verifik.app",
 	kycUrl: "https://sandbox-kyc.verifik.co",
 	verifikProject: "6266193db77ccc8111730c90",
