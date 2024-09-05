a1fe1f6062bd8da59eaef338aba83ea940ee4885
Miguel Trevino <miguel@verifik.co>
2024-09-05 14:51:22 -0500

    merge changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
 	// apiUrl: "https://api.verifik.co",
 	apiUrl: "https://verifik.app",
 	appUrl: "https://verifik.app",
-	kycUrl: "https://sandbox-kyc.verifik.co",
+	kycUrl: "https://sandbox.verifik.co",
 	verifikProject: "6266193db77ccc8111730c90",
 	sandboxUrl: "https://sandbox.verifik.co",
 	sandboxProject: "6266193db77ccc8111730c90",
