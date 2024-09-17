98cecddb2d664148be4576c1bdc6a9ca3a592ba9
Miguel Trevino <miguel@verifik.co>
2024-09-17 12:37:35 -0500

    dist

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -174,13 +174,11 @@ export class AuthSignUpComponent implements OnInit, OnDestroy {
 	 * On init
 	 */
 	ngOnInit(): void {
-		if (this._activatedRoute.params) {
-			this._activatedRoute.params?.subscribe((params) => {
-				this.isVerifikProject = Boolean(params.id === environment.verifikProject || params.id === environment.sandboxProject);
+		this._activatedRoute.params.subscribe((params) => {
+			this.isVerifikProject = Boolean(params.id === environment.verifikProject || params.id === environment.sandboxProject);
 
-				this.requestProject(params.id);
-			});
-		}
+			this.requestProject(params.id);
+		});
 
 		this._demoService.geoLocation$.subscribe({
 			next: async (response) => {
