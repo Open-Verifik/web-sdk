8d02b8bb9220806976f71cb7bdc4b8716b661f6d
migueltrevinom <miguel@verifik.co>
2025-01-25 16:05:12 -0500

    fix redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,52 +1,98 @@
-import { Route } from "@angular/router";
-import { AuthGuard } from "app/core/auth/guards/auth.guard";
-import { NoAuthGuard } from "app/core/auth/guards/noAuth.guard";
-import { LayoutComponent } from "app/layout/layout.component";
-import { environment } from "environments/environment";
+import { Route } from '@angular/router';
+import { AuthGuard } from 'app/core/auth/guards/auth.guard';
+import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
+import { LayoutComponent } from 'app/layout/layout.component';
+import { environment } from 'environments/environment';
 
-const defaultPath = `/sign-in/${environment.verifikProject}`;
+// if its https://demo-sandbox.verifik.co it should use sandboxProject
+// if its production it should use verifikProject
+// if its localhost it should use sandboxProject
+const defaultPath =
+    window.location.hostname.includes('localhost') ||
+    window.location.hostname.includes('demo-sandbox.verifik.co')
+        ? `/sign-in/${environment.sandboxProject}`
+        : `/sign-in/${environment.verifikProject}`;
 
 // @formatter:off
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const appRoutes: Route[] = [
-	{ path: "", pathMatch: "full", redirectTo: defaultPath },
-	{ path: "signed-in-redirect", pathMatch: "full", redirectTo: "demo" },
-	{
-		path: "",
-		canActivate: [NoAuthGuard],
-		canActivateChild: [NoAuthGuard],
-		component: LayoutComponent,
-		data: {
-			layout: "empty",
-		},
-		children: [
-			{ path: "confirmation-required", loadChildren: () => import("app/modules/auth/confirmation-required/confirmation-required.routes") },
-			{ path: "kyc", loadChildren: () => import("app/modules/auth/kyc-steps/kyc-steps.routes") },
-			{ path: "reset-password", loadChildren: () => import("app/modules/auth/reset-password/reset-password.routes") },
-			{ path: "sign-in", loadChildren: () => import("app/modules/auth/sign-in/sign-in.routes") },
-			{ path: "sign-up", loadChildren: () => import("app/modules/auth/sign-up/sign-up.routes") },
-		],
-	},
-	{
-		path: "",
-		canActivate: [AuthGuard],
-		canActivateChild: [AuthGuard],
-		component: LayoutComponent,
-		data: {
-			layout: "empty",
-		},
-		children: [
-			{ path: "sign-out", loadChildren: () => import("app/modules/auth/sign-out/sign-out.routes") },
-			{ path: "unlock-session", loadChildren: () => import("app/modules/auth/unlock-session/unlock-session.routes") },
-		],
-	},
-	{
-		path: "",
-		component: LayoutComponent,
-		data: {
-			layout: "empty",
-		},
-		children: [{ path: "demo", loadChildren: () => import("app/modules/demo/demo.routes") }],
-	},
+    { path: '', pathMatch: 'full', redirectTo: defaultPath },
+    { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'demo' },
+    {
+        path: '',
+        canActivate: [NoAuthGuard],
+        canActivateChild: [NoAuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'confirmation-required',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/confirmation-required/confirmation-required.routes'
+                    ),
+            },
+            {
+                path: 'kyc',
+                loadChildren: () =>
+                    import('app/modules/auth/kyc-steps/kyc-steps.routes'),
+            },
+            {
+                path: 'reset-password',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/reset-password/reset-password.routes'
+                    ),
+            },
+            {
+                path: 'sign-in',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-in/sign-in.routes'),
+            },
+            {
+                path: 'sign-up',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-up/sign-up.routes'),
+            },
+        ],
+    },
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'sign-out',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-out/sign-out.routes'),
+            },
+            {
+                path: 'unlock-session',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/unlock-session/unlock-session.routes'
+                    ),
+            },
+        ],
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'demo',
+                loadChildren: () => import('app/modules/demo/demo.routes'),
+            },
+        ],
+    },
 ];
