c38b27813bd54aa666531d7d51e55bd5819d0728
migueltrevinom <miguel@verifik.co>
2025-08-11 14:19:44 -0500

    push environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
     baseUrl: "/",
     apiUrl: "",
     appUrl: "https://app.verifik.co",
-    kycUrl: "https://kyc.verifik.co",
+    kycUrl: "https://access.verifik.co",
     verifikProject: "6332941ccde4f719d9c00f9e",
     stagingUrl: "https://staging.verifik.co",
-    sandboxUrl: "https://testing.verifik.co",
+    sandboxUrl: "https://staging.verifik.co",
     sandboxProject: "6266193db77ccc8111730c90",
 };
